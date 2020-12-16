3513c306b346c4a3679e5f1af5b2f10fd3c8c082
Chris Cooley <chriscooley1984@yahoo.com>
2020-12-15 21:27:38 -0700

    added multiple functions and consts

--- server.js
+++ server.js
@@ -66,5 +66,71 @@ function runApp(){
             }
         });
     
+    function viewEmployees(){
+        const query = "SELECT * FROM employee";
+        connection.query(query, function (err, res){
+            if (err) throw err;
+            console.log("\n\n List of Employees\n")
+            console.table(res);
+        });
+    }
+    
+    function viewRoles(){
+        const query = "SELECT * FROM role";
+        connection.query(query, function (err, res){
+            if (err) throw err;
+            console.log("\n\n List of Roles\n")
+            console.table(res);
+        });
+    }
+
+    function viewDepartments(){
+        const query = "SELECT * FROM department";
+        connection.query(query, function (err, res){
+            if (err) throw err;
+            console.log("\n\n List of Departments\n")
+            console.table(res);
+        });
+    }
+
+    function addEmployee(){
+        inquirer
+            .prompt([
+                {
+                    type: "input",
+                    message: "Enter the employee's first name.\n",
+                    name: "firstName",
+                },
+                {
+                    type: "input",
+                    message: "Enter the employee's last name.\n",
+                    name: "lastName",
+                },
+                {
+                    type: "input",
+                    message: "What is the employee's role ID?\n",
+                    name: "roleID",
+                },
+                {
+                    type: "input",
+                    message: "What is the employee's manager ID?\n",
+                    name: "managerID",
+                },
+            ])
+
+            .then(function (res){
+                const firstName = res.firstName;
+                const lastName = res.lastName;
+                const roleID = res.roleID;
+                const managerID = res.managerID;
+                const query = `INSERT INTO employee (first_name, last_name, role_id, manager_id) VALUE("${firstName}", "${lastName}", "${roleID}", "${managerID}")`;
+                connection.query(query, function (err, res){
+                    if (err) throw err;
+                    console.table(res);
+                    runApp;
+                });
+            });
+    }
+
     
 }
