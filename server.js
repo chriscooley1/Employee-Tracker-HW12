c9040f637cc98900632eb7a1a350ccf4c300cb0f
Chris Cooley <chriscooley1984@yahoo.com>
2020-12-15 20:41:43 -0700

    added function runApp with prompts and .then info

--- server.js
+++ server.js
@@ -16,3 +16,55 @@ connection.connect(function (err){
     console.log("connected as id " + connection.threadId + "\n");
 });
 
+function runApp(){
+    inquirer
+        .prompt({
+            name: "menu",
+            type: "list",
+            message: "what do you want to do?\n",
+            choices: [
+                "View Employees",
+                "View Roles",
+                "View Departments",
+                "Add Employee",
+                "Add Role",
+                "Add Department",
+                "Update Employee's Role",
+                "Exit",
+            ],
+        })
+        .then(function (res){
+            console.log("You entered: " + res.menu);
+            switch (res.menu){
+                case "View Employees":
+                    viewEmployees();
+                    runApp();
+                    break;
+                case "View Roles":
+                    viewRoles();
+                    runApp();
+                    break;
+                case "View Departments":
+                    viewDepartments();
+                    runApp();
+                    break;
+                case "Add Employee":
+                    addEmployee();
+                    break;
+                case "Add Role":
+                    addRole();
+                    break;
+                case "Add Department":
+                    addDepartment();
+                    break;
+                case "Update Employee's Role":
+                    updateEmployeeRole();
+                    break;
+                case "Exit":
+                    connection.end();
+                    break;
+            }
+        });
+    
+    
+}
